b6a923b82206ae6626bf70517e7e13227cbe0a87
ShavitIdan <idanshavit5@gmail.com>
2024-07-22 13:24:09 +0300

    Player movement + animations
    completed player movement and the animations

--- Adventure Island/Assets/Scripts/SC_Movement.cs
+++ Adventure Island/Assets/Scripts/SC_Movement.cs
@@ -4,52 +4,45 @@ using UnityEngine;
 
 public class SC_Movement : MonoBehaviour
 {
-    public float speed = 5;
-    private float direction;
-    public float jumpSpeed = 100f;
-    private bool isGrounded = false;
-    private Rigidbody2D rigid;
-    public LayerMask groundLayer;
+    public float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
-    void Awake()
+    private void Awake()
     {
-        rigid = GetComponent<Rigidbody2D>();
-    }
-    void FixedUpdate()
-    {
-        direction = Input.GetAxis("Horizontal");
-        if (direction != 0 && rigid != null)
-        {
-            rigid.velocity = new Vector2(direction * speed, rigid.velocity.y);
-
-            if (direction > 0)
-                transform.localScale = new Vector3(1, 1, 1);
-            else transform.localScale = new Vector3(-1, 1, 1);
-        }
-
-        isGrounded = transform.IsGrounded(groundLayer);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (isGrounded)
-            {
-                Jump();
-            }
-        }
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    public float GetSpeed()
+    private void Update()
     {
-        return speed;
-    }
-    public void SetSpeed(float newSpeed)
-    {
-        speed = newSpeed;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        anim.SetBool("IsRunning", horizontalInput != 0);
+        anim.SetBool("IsGrounded", grounded);
     }
 
     private void Jump()
     {
-        rigid.velocity = new Vector2(rigid.velocity.x, 0);
-        rigid.AddForce(new Vector2(0, jumpSpeed));
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("Jump");
+        grounded = false;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+
 }
